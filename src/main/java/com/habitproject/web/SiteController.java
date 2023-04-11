f3e688c7399d59ae5d827cbc2206e94d098a7e8f
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-04-11 10:21:30 +0200

    deleted old files and code

--- src/main/java/com/habitproject/web/SiteController.java
+++ src/main/java/com/habitproject/web/SiteController.java
@@ -1,6 +1,6 @@
 package com.habitproject.web;
 
-import com.habitproject.configuration.Endpoints;
+import com.habitproject.configuration.SecurityConfig;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,27 +9,27 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class SiteController {
 
-    @RequestMapping(path = Endpoints.Site.SLASH)
+    @RequestMapping(path = SecurityConfig.Site.SLASH)
     public ModelAndView showIndexPage() {
         return new ModelAndView("home.html");
     }
 
-    @GetMapping(path = Endpoints.Site.INDEX)
+    @GetMapping(path = SecurityConfig.Site.INDEX)
     public ModelAndView showIndexPage2() {
         return new ModelAndView("home");
     }
 
-    @GetMapping(path = Endpoints.Site.DASH)
+    @GetMapping(path = SecurityConfig.Site.DASH)
     public ModelAndView showDashboard() {
         return new ModelAndView("dashboard");
     }
 
-    @GetMapping(path = Endpoints.Site.ERROR)
+    @GetMapping(path = SecurityConfig.Site.ERROR)
     public ModelAndView showError() {
         return new ModelAndView("error");
     }
 
-    @GetMapping(path = Endpoints.Site.LOGIN)
+    @GetMapping(path = SecurityConfig.Site.LOGIN)
     public ModelAndView showLogin() {
         return new ModelAndView("login");
     }
