9157c41c03e9e3dd71747d757f6896b308a304eb
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-04-26 21:23:12 +0200

    added controller, html sites

--- src/main/java/tech/ignitr/habitus/web/SiteController.java
+++ src/main/java/tech/ignitr/habitus/web/SiteController.java
@@ -1,33 +1,35 @@
 package tech.ignitr.habitus.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class SiteController {
 
     @RequestMapping(path = "/")
     public ModelAndView showIndexPage() {
-        return new ModelAndView("home.html");
+        return new ModelAndView("index.html");
     }
 
     @GetMapping(path = "/index")
     public ModelAndView showIndexPage2() {
-        return new ModelAndView("home");
+        return new ModelAndView("index.html");
     }
 
     @GetMapping(path = "/dashboard")
     public ModelAndView showDashboard() {
-        return new ModelAndView("dashboard");
+        return new ModelAndView("dashbaord.html");
     }
 
     @GetMapping(path = "error")
     public ModelAndView showError() {
-        return new ModelAndView("error");
+        return new ModelAndView("error.html");
     }
 
     @GetMapping(path = "login")
     public ModelAndView showLogin() {
-        return new ModelAndView("login");
+        return new ModelAndView("login.html");
     }
 }
